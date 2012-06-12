38ac817bc8c4a2548811e780ce00602477dadb90
Al Scott <scottaj2@udmercy.edu>
2012-06-12 19:34:50 -0400

    Working development

--- lib/rcr.rb
+++ lib/rcr.rb
@@ -1,7 +1,18 @@
 require 'mongoid'
 
+
+class RoomItem
+  include Mongoid::Document
+  field :category, type: String
+  field :item, type: String
+  field :condition, type: Integer
+  field :condition_notes, type: String
+  embedded_in :rcr
+end
+
 class RCR
   include Mongoid::Document
+  field :token, type: String
   field :term_year, type: Integer
   field :term_name, type: String
   field :first_name, type: String
@@ -10,5 +21,10 @@ class RCR
   field :building, type: String
   field :room_number, type: Integer
   field :complete, type: Boolean
-  embeds_many :roomitems  
+  embeds_many :room_items
+
+  def RCR.token_exists_for_term?(token, term_year, term_name)
+    return true if RCR.where(token: token, term_year: term_year, term_name: term_name).first
+    return false
+  end
 end
--- main.rb
+++ main.rb
@@ -2,20 +2,52 @@ require 'bundler/setup'
 require 'sinatra'
 require 'slim'
 require 'mongoid'
+require 'yaml'
+
+require_relative 'lib/rcr'
+require_relative 'lib/room_info'
 
 class RCRApp < Sinatra::Base
   configure do
-    set :environment, :development
+    enable :sessions
+    #set :environment, :development
     # Mongoid.load!("./mongoid.yml")
     Mongoid.configure do |config|
       name = "rcr_app"
       host = "localhost"
       config.master = Mongo::Connection.new.db(name)
     end
+
+    Slim::Engine.set_default_options :pretty => true
+  end
+
+  helpers do
+    def generate_token(n)
+      return rand(36**n).to_s(36)
+    end
+  end
+
+  before /\/admin.*/ do
+    
+  end
+
+  before '/submit' do
+    
   end
 
   get '/' do
-    
+    slim :index, locals: {page_title: "Sign In", message: nil}
+  end
+
+  post '/' do
+    session[:active_term] = nil
+    File.open('active.yml', 'r') {|f| active_term = YAML::load(f)}
+    if RCR.token_exists_for_term?(params[:token], session[:active_term][:year], session[:active_term][:term])
+      session[:token] = params[:token]
+      redirect '/submit'
+    else
+      slim :index, locals: {page_title: "Sign In", message: "Token not found!"}
+    end
   end
 
   get '/submit' do
@@ -32,4 +64,4 @@ class RCRApp < Sinatra::Base
 end
 
 
-RCRApp.run if __FILE__ == $0
+RCRApp.run! if __FILE__ == $0
