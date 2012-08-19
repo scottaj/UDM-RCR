4de65526406492163eea31b9ccf410f907003d10
alexander <alexander@vbox.fedora>
2012-08-19 15:21:25 -0400

    Added functionality to the AreaMapping class.

--- lib/area_mapping.rb
+++ lib/area_mapping.rb
@@ -1,5 +1,8 @@
 require 'set'
 
+class MappingError < StandardError
+end
+
 class Item
   include Mongoid::Document
   field :category, type: String
@@ -12,7 +15,7 @@ class Area
   include Mongoid::Document
   field :name, type: String
   embeds_many :items
-  has_and_belongs_to_many :areamappings, class_name: "AreaMapping"
+  has_and_belongs_to_many :mappings, inverse_of: :areas, class_name: "AreaMapping"
 end
 
 ##
@@ -21,20 +24,50 @@ class AreaMapping
   include Mongoid::Document
   field :name, type: String
   field :building, type: String
-  field :rooms, type: Array
-  has_and_belongs_to_many :areas
+  field :rooms, type: Set
+  has_and_belongs_to_many :areas, inverse_of: :mappings, class_name: "Area"
+
+  attr_accessor :conflicts
+
+  def check_for_duplicates()
+    duplicate = AreaMapping.where(building: self.building).in(rooms: self.rooms)
+    
+    unless duplicate.empty?
+      @conflicts = {}
+      duplicate.each do |mapping|
+        @conflicts[mapping.name] = self.rooms & mapping.rooms
+      end
+      raise MappingError, "This mapping conflicts with an existing mapping"
+    end
+  end
+
+  def save_areas()
+    self.areas.each {|area| area.save}
+  end
+  
+  before_save do
+    check_for_duplicates
+    save_areas
+  end
+
+  def self.get_area_names_for_room(building, room)
+    mapping = self.where(building: building, rooms: room).first
+    return Set[] unless mapping
+    return Set.new(mapping.areas.map {|area| area.name})
+  end
   
   def self.get_items_for_room(building, room_number)
-    items = []
-    mapping = AreaMapping.find_by(building: building, rooms: room_number)
-    mapping.areas.each do |area|
-      items += area.items
-    end
-    return items
+    mapping = AreaMapping.where(building: building, rooms: room_number).first
+    return Set[] unless mapping
+    return Set.new(mapping.areas.map {|a| Set.new(a.items)}).flatten
+  end
+
+  def self.get_category_names_for_room(building, room_number)
+    return self.get_items_for_room(building, room_number).map! {|item| item.category}
   end
 
   def map_rooms(room_number_string)
-    rooms = Set.new
+    rooms = Set[]
     room_number_string.split(/\s*,\s*/).each do |section|
       if section.match(/^[0-9]+-[0-9]+$/)
         section = section.split('-')
@@ -43,7 +76,6 @@ class AreaMapping
         rooms << section.to_i if section.match(/^[0-9]+$/)
       end
     end
-    self.rooms = rooms.to_a
-    self.save
+    self.rooms = rooms
   end
 end
--- spec/area_mapping_spec.rb
+++ spec/area_mapping_spec.rb
@@ -1,27 +1,112 @@
 require 'spec_helper'
 
 describe "AreaMapping" do
-  describe "map_rooms" do
-    before do
-      @am = AreaMapping.new
-    end
-  
-    it "should convert ranges of rooms from a string into an array" do
-      room_str = "1-4,17,18,23-24"
-      @am.map_rooms(room_str)
-      @am.rooms.should =~ [1,2,3,4,17,18,23,24]
+  describe "Mapping rooms to areas" do
+
+    after do
+      AreaMapping.destroy_all
+      Area.destroy_all
     end
 
-    it "should ignore junk in the data string" do
-      room_str = "moose,3-4hi,2, 9-yo-11        ,w-8"
-      @am.map_rooms(room_str)
-      @am.rooms.should =~ [2]
+    it "should save any areas in the mapping when the mapping is saved" do
+      am = AreaMapping.new(name: "test", building: "test", rooms: Set[1,2,3,4,5])
+      a = Area.new(name: "area1")
+      am.areas << a
+      
+      a.persisted?.should be_false
+      am.save
+      a.persisted?.should be_true      
+    end
+    
+    describe "Finding area names for a mapping" do
+
+      it "should return a set of the area names that a room is in" do
+        am = AreaMapping.new(name: "test", building: "test", rooms: Set[1,2,3,4,5])
+        am.areas = [Area.new(name: "area1"), Area.new(name: "area2"), Area.new(name: "area3")]
+        am.save
+        AreaMapping.get_area_names_for_room("test", 3).should == Set["area1",
+                                                                     "area2",
+                                                                     "area3"]
+      end
+
+      it "should return an empty set if it can't find a mapping for a room" do
+        AreaMapping.get_area_names_for_room("foo", 8).should == Set[]
+      end
     end
 
-    it "should not contain duplicates" do
-      room_str = "1-5,2-6,3,4"
-      @am.map_rooms(room_str)
-      @am.rooms.should =~ [1,2,3,4,5,6]
+    describe "Creating mappings" do
+      
+      it "should not allow you to save multiple mappings for a single room" do     
+        lambda {
+          AreaMapping.create(name: "test1", building: "test", rooms: Set[1,2,3])
+        }.should_not raise_error
+        lambda {
+          AreaMapping.create(name: "test2", building: "test", rooms: Set[3,4,5])
+        }.should raise_error(MappingError)
+      end
+
+      it "Should set the conflicts hash if there is a conflict while saving" do
+        am1 = AreaMapping.create(name: "test", building: "test", rooms: Set[1,2,3])
+        am2 = AreaMapping.create(name: "test2", building: "test", rooms: Set[6,7,8])
+        am3 = AreaMapping.new(name: "test3", building: "test", rooms: Set[3,4,5,6])
+        lambda {am3.save}.should raise_error
+        am3.conflicts.should == {"test" => Set[3], "test2" => Set[6]}
+     
+      end
+    end
+
+    describe "Getting item data for a given room" do
+
+      before do
+        @area1 = Area.new(name: "test area 1")
+        @area1.items = [Item.new(category: "Structural", name: "Floor"),
+                       Item.new(category: "Structural", name: "Ceiling"),
+                       Item.new(category: "Furniture", name: "Bed")]
+        @area2 = Area.new(name: "test area 2")
+        @area2.items = [Item.new(category: "Furniture", name: "Chair"),
+                       Item.new(category: "Bathroom", name: "Toilet")]
+        @am = AreaMapping.create(name: "test mapping",
+                                building: "test",
+                                rooms: Set[1,2,3,4,5],
+                                areas: [@area1, @area2])
+
+      end
+      
+      it "should return a set of all the items in a given room." do        
+        AreaMapping.get_items_for_room("test", 2).should == Set.new(@area1.items) | Set.new(@area2.items)
+      end
+
+      it "should return an empty set if no mapping can be found for a room" do
+        AreaMapping.get_items_for_room("fahrenheit", 451).should == Set[]
+      end
+
+      describe "Getting category information for items in a given room" do
+        it "should return a set with the names of every category of items in the room" do
+          AreaMapping.get_category_names_for_room("test", 3).should == Set["Structural", "Furniture", "Bathroom"]
+        end
+
+        it "should return an empty set if no mapping can be found for a room" do
+          AreaMapping.get_category_names_for_room("fahrenheit", 451).should == Set[]
+        end
+      end
+    end
+    
+    describe "Map string ranges to actual numbers" do
+      before do
+        @am = AreaMapping.new
+      end
+      
+      it "should convert ranges of rooms from a string into an Set" do
+        room_str = "1-4,17,18,23-24"
+        @am.map_rooms(room_str)
+        @am.rooms.should == Set[1,2,3,4,17,18,23,24]
+      end
+
+      it "should ignore junk in the data string" do
+        room_str = "moose,3-4hi,2, 9-yo-11        ,w-8"
+        @am.map_rooms(room_str)
+        @am.rooms.should == Set[2]
+      end
     end
   end
 end
