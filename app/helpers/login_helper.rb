bf29791f1f6e009f9afcc5ab694ff14aa7c9189c
alexander <alexander@vbox.fedora>
2012-09-04 10:45:24 -0400

    Moved method from the login helper to the rcr helper.

--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -4,8 +4,4 @@ RcrApp.helpers do
   def find_rcr_with_token(token)
     return Term.current_term.rcrs.find_by(token: token)
   end
-
-  def session_rcr()
-    find_rcr_with_token(session[:token])
-  end
 end
