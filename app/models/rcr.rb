719c90dbcad004cfbd8a447b0c063e9195d6d8c4
alexander <alexander@vbox.fedora>
2012-09-04 23:22:22 -0400

    Removed dead code.

--- app/models/rcr.rb
+++ app/models/rcr.rb
@@ -23,32 +23,4 @@ class RCR
   belongs_to :term, class_name: "Term"
 
   alias :complete? :complete
-
-  # def self.token_exists_for_term?(token, term_year, term_name)
-  #   return true if RCR.where(token: token, term_year: term_year, term_name: term_name).first
-  #   return false
-  # end
-
-  # def self.get_rcr_for_term_by_token(token, term_year, term_name)
-  #   return RCR.where(token: token, term_year: term_year, term_name: term_name).first
-  # end
-
-  # def update_or_create_room_item(room_item_params)
-  #   found = self.room_items.where(name: room_item_params[:name]).first
-  #   if found
-  #     found.update_attributes(room_item_params)
-  #     self.save
-  #   else
-  #     self.room_items.create(room_item_params)
-  #   end
-  # end
-
-  # def get_rated_items()
-  #   return self.room_items.map {|item| item.name if (1..5).include?(item.rating)}
-  # end
-
-  # def mark_complete()
-  #   self.update_attribute(:complete, true)
-  #   self.save
-  # end
 end
